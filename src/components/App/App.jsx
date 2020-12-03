5d43873433a01090c7801410d7910c48c8e098e6
Ruben de Bruijn <ruben.de.bruijn@carcollect.com>
2020-12-03 14:03:48 +0100

    boiler plate clean-up

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import logo from '../../assets/logo.svg';
+
+const App = () => (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <img src={logo} height={500} width={500} className="App-logo" alt="logo" />
+    </div>
+  );
+
+export default App;
